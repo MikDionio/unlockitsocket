25e879a9faa34b76550dcd85fb6911e6e45d4e52
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-05-05 11:46:03 +0800

    deployment setup

--- unlockit_socket/settings.py
+++ unlockit_socket/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,3 +159,6 @@ JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
     'JWT_AUTH_COOKIE': None
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
