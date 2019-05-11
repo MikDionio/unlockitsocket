1ac1fd31d4e3965ec686211a2fc038ce280a925b
Jonathan Cancio <jonahcancio@gmail.com>
2019-05-11 15:10:07 +0800

    made balance positive int

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     rfid_number = models.BigIntegerField(primary_key=True)
     student_number = models.BigIntegerField(null=True)    
     name = models.CharField(max_length=100)
-    balance = models.IntegerField(default=0)    
+    balance = models.PositiveIntegerField(default=0)    
     is_using = models.BooleanField(default=False)
     last_login = models.DateTimeField(null=True, editable=False)
     last_logout = models.DateTimeField(null=True, editable=False)
@@ -27,8 +27,8 @@ class Student(models.Model):
                 print("LOG OUT")
                 self.last_logout = timezone.now()
                 print("CONSUMING BALANCE...")
-                self.balance -= (self.last_logout - self.last_login).total_seconds()
-                self.balance = 0 if self.balance < 0 else self.balance
+                new_balance = self.balance - (self.last_logout - self.last_login).total_seconds()
+                self.balance = 0 if new_balance < 0 else new_balance
 
             # balance
             old_balance = old_student.balance
